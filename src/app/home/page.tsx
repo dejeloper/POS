a4045582fe234bd162dce0d2de46e9b916095ac4
Jhonatan Guerrero <jhonatanguerrero@outlook.com>
2024-08-22 23:21:24 -0500

    feat: Add Dashboard principal

---
+++ src/app/home/page.tsx
@@ -0,0 +1,61 @@
+import Link from "next/link";
+import { buttonVariants } from "@/components/ui/button";
+import { cn } from "@/lib/utils";
+
+export default function HomePage() {
+  const title = "Hola 👋 a GENN";
+  const description =
+    "GENN es un sistema de punto de venta diseñado para optimizar la gestión de tu negocio. Con GENN, puedes realizar ventas, gestionar inventarios, y llevar un control detallado de las transacciones y reportes. GENN te ayuda a simplificar la operación diaria y a maximizar la eficiencia de tu negocio.";
+
+  const calltoAction =
+    "Revisa tus objetivos y tareas por cumplir en la página 'Diario'.";
+
+  const buttons = [
+    {
+      name: "Crear Objetivo",
+      link: "/objectives/new",
+    },
+    {
+      name: "Crear Tarea",
+      link: "/tasks/new",
+    },
+    {
+      name: "Diario",
+      link: "/daily",
+    },
+  ];
+
+  return (
+    // <div className="flex flex-col justify-center m-4 ">
+    //   <h1 className="mb-4 text-4xl font-extrabold text-center leading-none tracking-tight text-foreground md:text-5xl lg:text-6xl  ">
+    //     {title}
+    //   </h1>
+    //   <p className="mt-4 text-lg text-left text-foreground lg:mx-auto lg:w-3/4 2xl:w-2/3">
+    //     {description}
+    //   </p>
+
+       <div className="flex flex-col justify-center items-center mt-8 max-w-[500px] mx-auto">
+        <div className="flex flex-col xs:flex-row justify-center xs:w-auto w-full mx-auto">
+          {buttons &&
+            buttons.map((button) => (
+              <Link
+                key={button.name}
+                className={cn(
+                  buttonVariants({ variant: "default" }),
+                  "py-2.5 px-5 me-2 mb-2 text-sm font-medium"
+                )}
+                href={button.link}
+              >
+                {button.name}
+              </Link>
+            ))}
+        </div>
+        <p className="mt-4 text-md text-left text-foreground">{calltoAction}</p>
+      </div> 
+    </div> 
+
+    <div className=" grid grid-cols-1 lg:grid-cols-2">
+
+    </div>
+  );
+}
