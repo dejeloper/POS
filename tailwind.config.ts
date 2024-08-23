aa2e7c500dbd2e0413163b7ab79ca0019ca0e8d6
Jhonatan Guerrero <jhonatanguerrero@outlook.com>
2024-08-23 11:38:02 -0500

    style: Add success, info and warning colors

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -56,6 +56,18 @@ const config = {
           DEFAULT: "hsl(var(--card))",
           foreground: "hsl(var(--card-foreground))",
         },
+        success: {
+          DEFAULT: "hsl(var(--success))",
+          foreground: "hsl(var(--success-foreground))",
+        },
+        info: {
+          DEFAULT: "hsl(var(--info))",
+          foreground: "hsl(var(--info-foreground))",
+        },
+        warning: {
+          DEFAULT: "hsl(var(--warning))",
+          foreground: "hsl(var(--warning-foreground))",
+        },
       },
       borderRadius: {
         lg: "var(--radius)",
