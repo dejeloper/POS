baa379057623a82d7d258fa7431869dd9792d256
Jhonatan Guerrero <jhonatanguerrero@outlook.com>
2024-08-23 11:57:36 -0500

    feat: Add Auth Register Page

---
+++ src/app/auth/register/page.tsx
@@ -0,0 +1,159 @@
+"use client";
+
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { userRegisterSchema } from "@/schemas/users/register.schemas";
+import { useRouter } from "next/navigation";
+import { z } from "zod";
+
+import { Button } from "@/components/ui/button";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { toast } from "@/components/ui/use-toast";
+
+export default function RegisterPage() {
+  const form = useForm<z.infer<typeof userRegisterSchema>>({
+    resolver: zodResolver(userRegisterSchema),
+    defaultValues: {
+      email: "jhonatanguerrero@outlook.com",
+      username: "admin",
+      password: "admin123",
+      confirmPassword: "admin123",
+    },
+  });
+
+  const router = useRouter();
+
+  async function onSubmit(dataNewUser: z.infer<typeof userRegisterSchema>) {
+    const res = await fetch("/api/auth/register", {
+      method: "POST",
+      body: JSON.stringify({
+        email: dataNewUser.email,
+        username: dataNewUser.username,
+        password: dataNewUser.password,
+      }),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+
+    const { message, success } = await res.json();
+
+    toast({
+      variant: success ? "success" : "warning",
+      description: message,
+    });
+
+    if (res.ok) {
+      router.push("/auth/login");
+    }
+  }
+
+  return (
+    <>
+      <Card className="mx-auto max-w-sm">
+        <CardHeader>
+          <CardTitle className="text-xl">Registro</CardTitle>
+          <CardDescription>
+            Ingrese su información para crear una cuenta
+          </CardDescription>
+        </CardHeader>
+        <CardContent>
+          <Form {...form}>
+            <form onSubmit={form.handleSubmit(onSubmit)}>
+              <div className="grid gap-4">
+                <div className="grid gap-2">
+                  <FormField
+                    control={form.control}
+                    name="email"
+                    render={({ field }) => (
+                      <FormItem>
+                        <FormLabel>Email</FormLabel>
+                        <FormControl>
+                          <Input
+                            type="email"
+                            placeholder="you@example.com"
+                            {...field}
+                          />
+                        </FormControl>
+                        <FormMessage />
+                      </FormItem>
+                    )}
+                  />
+
+                  <FormField
+                    control={form.control}
+                    name="username"
+                    render={({ field }) => (
+                      <FormItem>
+                        <FormLabel>Usuario</FormLabel>
+                        <FormControl>
+                          <Input placeholder="Max" {...field} />
+                        </FormControl>
+                        <FormMessage />
+                      </FormItem>
+                    )}
+                  />
+
+                  <FormField
+                    control={form.control}
+                    name="password"
+                    render={({ field }) => (
+                      <FormItem>
+                        <FormLabel>Contraseña</FormLabel>
+                        <FormControl>
+                          <Input
+                            type="password"
+                            placeholder="********"
+                            {...field}
+                          />
+                        </FormControl>
+                        <FormMessage />
+                      </FormItem>
+                    )}
+                  />
+
+                  <FormField
+                    control={form.control}
+                    name="confirmPassword"
+                    render={({ field }) => (
+                      <FormItem>
+                        <FormLabel>Confirmar Contraseña</FormLabel>
+                        <FormControl>
+                          <Input
+                            type="password"
+                            placeholder="********"
+                            {...field}
+                          />
+                        </FormControl>
+                        <FormMessage />
+                      </FormItem>
+                    )}
+                  />
+                </div>
+                <Button type="submit" className="w-full">
+                  Registrarse
+                </Button>
+              </div>
+            </form>
+          </Form>{" "}
+        </CardContent>
+      </Card>
+    </>
+  );
+}
