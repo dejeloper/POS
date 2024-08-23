980325575f94fa4e1676a36df32bd4e39daf687e
Jhonatan Guerrero <jhonatanguerrero@outlook.com>
2024-08-23 18:19:20 -0500

    feat: Add Loading Component

---
+++ src/app/loading.tsx
@@ -0,0 +1,15 @@
+import { Skeleton } from "@/components/ui/skeleton";
+
+export default function Loading() {
+  return (
+    <div className="flex justify-center items-center h-[calc(100vh-168px)]">
+      <div className="flex flex-row bg-primary shadow-lg border-r-4 w-[20rem] items-center gap-2 p-2 rounded-xl">
+        <Skeleton className="rounded-full w-[4.5rem] h-[4.5rem]" />
+        <div className="flex flex-col gap-2 w-9/12">
+          <Skeleton className="w-11/12 h-2 rounded-full" />
+          <Skeleton className="w-9/12 h-2 rounded-full" />
+        </div>
+      </div>
+    </div>
+  );
+}
