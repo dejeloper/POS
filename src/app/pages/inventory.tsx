d015fe708708d7950a5a2ca995de1effcd515f86
Jhonatan Guerrero <jhonatanguerrero@outlook.com>
2024-08-23 22:15:15 -0500

    feat: Add sales e inventory Page and 404 Page

---
+++ src/app/pages/inventory.tsx
@@ -0,0 +1,11 @@
+export default function InventoryPage() {
+  return (
+    <div className="flex justify-center items-center h-[calc(100vh-168px)]">
+      <div className="flex flex-col justify-center m-4 ">
+        <h1 className="mb-4 text-4xl font-extrabold text-center leading-none tracking-tight text-foreground md:text-5xl lg:text-6xl  ">
+          Inventario
+        </h1>
+      </div>
+    </div>
+  );
+}
